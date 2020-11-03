d35e4c2e2644a8ca3281e5ce81faa94e7a1c83d7
ValeriiaZhubrieva <perchik010102@gmail.com>
2020-11-03 22:37:45 +0200

    added home-page and about-page

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    
+    $('.reviews__inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots:true,
+        arrows:false,
+    });
+
+});
