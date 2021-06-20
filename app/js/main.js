95c6415749511e00c94f399abdf94a892cebaa31
ValeriiaZhubrieva <perchik010102@gmail.com>
2021-06-20 21:41:05 +0300

    add doctors-page and one-service-page

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,14 @@ $(function () {
         arrows:false,
     });
 
+
+    $('.doctors__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.doctors__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.doctors__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
 });
